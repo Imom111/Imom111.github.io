d9e7e066cff75657d635ffd9ab007f58a376be06
Imom111 <imom11ivan@gmail.com>
2023-08-13 01:39:39 -0600

    feat: fisrt building and deploying

---
+++ build/_app/immutable/chunks/paths.339eea22.js
@@ -0,0 +1,1 @@
+var s;const e=((s=globalThis.__sveltekit_17ad7iu)==null?void 0:s.base)??"/slick-portfolio-svelte";var a;const t=((a=globalThis.__sveltekit_17ad7iu)==null?void 0:a.assets)??e;export{t as a,e as b};
